edd11974c0a0405b481f64f5f035dd014d9ce3f5
deividrw <94568028+deividrw@users.noreply.github.com>
2023-12-02 14:46:36 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
+import { useState } from "react";
+import api from "./services/api";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [cep,setCep] = useState({})
+ 
+
+  async function handleSearch() {
+    try {
+      if (input === "") {
+        alert("Preencha algum CEP");
+        return;
+      }
+
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("")
+    } catch (error) {
+      console.error("Erro ao buscar:", error);
+      alert("Erro ao buscar");
+      setInput("")
+    }
+   
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador Cep</h1>
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu Cep"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+
+      {Object.keys(cep).length > 0 && (
+
+      <main className="main">
+        <h2>CEP: {cep.cep}</h2>
+        <span>{cep.logradouro}</span>
+        <span>Complemento:{cep.complemento} </span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade} - {cep.uf}</span>
+      </main>
+  )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const api =  axios.create({
+    baseURL:"https://viacep.com.br/ws/"
+})
+
+
+
+
+export default api;
+
